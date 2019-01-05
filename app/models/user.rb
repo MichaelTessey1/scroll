41fc98b052ad854bb5cb737299df3a8c60cc8fe3
soleilyasmina <mounlasolomon@gmail.com>
2019-01-05 13:59:12 -0500

    completed authentication for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,14 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true
   
+  has_many :comments
+  has_many :posts
+
   def to_token_payload
     {
       sub: id,
       email: email
     }
   end
-  
-  has_many :comments
-  has_many :posts
   
 end
