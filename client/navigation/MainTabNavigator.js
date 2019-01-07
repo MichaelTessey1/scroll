32452e9fb268193c918abedd2ea2d9e010adf91d
soleilyasmina <mounlasolomon@gmail.com>
2019-01-07 00:37:56 -0500

    added tab to navigator

--- client/navigation/MainTabNavigator.js
+++ client/navigation/MainTabNavigator.js
@@ -3,10 +3,28 @@ import { Platform } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
+import CameraScreen from '../screens/CameraScreen';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
+const CameraStack = createStackNavigator({ // configures views
+  Camera: CameraScreen
+})
+
+CameraStack.navigationOptions = {
+  tabBarLabel: 'Camera', // name of tab
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={ // conditionally renders based on platform and focus
+        Platform.OS === 'ios'
+          ? 'ios-camera'
+          : 'md-camera'
+      }
+    />
+  )
+}
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
 });
@@ -54,6 +72,7 @@ SettingsStack.navigationOptions = {
 };
 
 export default createBottomTabNavigator({
+  CameraStack,
   HomeStack,
   LinksStack,
   SettingsStack,
