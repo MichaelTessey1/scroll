49ad58309a013acf7da96f0ab5c67fdbf7171a26
soleilyasmina <mounlasolomon@gmail.com>
2019-01-08 17:56:25 -0500

    rendered

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  Dimensions,
   Image,
   Platform,
   ScrollView,
@@ -9,17 +10,37 @@ import {
   View,
 } from 'react-native';
 import { WebBrowser } from 'expo';
-
+import axios from 'axios';
 import { MonoText } from '../components/StyledText';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     title: "Feed" 
   };
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: ''
+    }
+    this.getPhotos = this.getPhotos.bind(this);
+  }
+  getPhotos = async () => {
+    const resp = await axios.get('http://3e4a7a98.ngrok.io/posts');
+    return resp.data;
+  }
+  componentDidMount = async () => {
+    let photos = await this.getPhotos();
+    await this.setState({photos});
+  }
   render() {
+    let { height, width } = Dimensions.get('window');
     return (
-      <View>
-        
+      <View style={{flex: 1, flexDirection: 'row', flexWrap: 'wrap'}}>
+        {this.state.photos ? this.state.photos.map(photo => {
+          return (
+            <Image source={{uri: photo.photo}} style={{height: width / 3, width: width / 3}}/>
+          )
+        }): null}  
       </View>
     );
   }
