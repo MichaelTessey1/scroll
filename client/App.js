46a2a7630537ff52ff0e1a7efe97bf63fa45b6e6
soleilyasmina <mounlasolomon@gmail.com>
2019-01-09 19:06:21 -0500

    changed splash screen, adjusted app state

--- client/App.js
+++ client/App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       isLoadingComplete: false,
-      photos: '',
+      token: '' ,
     }
   }
 
@@ -33,10 +33,6 @@ export default class App extends React.Component {
 
   _loadResourcesAsync = async () => {
     return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         ...Icon.Ionicons.font,
